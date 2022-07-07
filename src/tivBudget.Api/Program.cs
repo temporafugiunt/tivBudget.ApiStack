5e14187069db2a90b6c6d20cd9d83e124d85f7ce
James Eby <admin@freebytech.com>
2022-07-06 21:41:36 -0500

    Updated to support better display logging in stdout.

--- src/tivBudget.Api/Program.cs
+++ src/tivBudget.Api/Program.cs
@@ -12,58 +12,58 @@ using freebyTech.Common.ExtensionMethods;
 
 namespace tivBudget.Api
 {
+  /// <summary>
+  /// Main entry point for tivBuget.Api application.
+  /// </summary>
+  public class Program
+  {
     /// <summary>
-    /// Main entry point for tivBuget.Api application.
+    /// The static ExecutionEnvironment defined for the application.
     /// </summary>
-    public class Program
+    public static IExecutionEnvironment ExecutionEnvironment = new ExecutionEnvironment(new EnvironmentManager());
+
+    /// <summary>
+    /// The static Configuraiton definde for the application.
+    /// </summary>
+    public static IConfiguration Configuration { get; } = new ConfigurationBuilder().AddDefaultConfiguration(ExecutionEnvironment).Build();
+
+
+    /// <summary>
+    /// Main entry point for application.
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static int Main(string[] args)
     {
-        /// <summary>
-        /// The static ExecutionEnvironment defined for the application.
-        /// </summary>
-        public static IExecutionEnvironment ExecutionEnvironment = new ExecutionEnvironment(new EnvironmentManager());
-        
-        /// <summary>
-        /// The static Configuraiton definde for the application.
-        /// </summary>
-        public static IConfiguration Configuration { get; } = new ConfigurationBuilder().AddDefaultConfiguration(ExecutionEnvironment).Build();
+      Log.Logger = new LoggerConfiguration()
+          .ReadFrom.Configuration(Configuration)
+          .Enrich.FromLogContext()
+          .WriteTo.Console() // outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+          .WriteTo.ApplicationInsightsWithStandardLoggersForTraceTelemetry(Configuration["ApplicationInsights.InstrumentationKey"])
+          .CreateLogger();
 
-
-        /// <summary>
-        /// Main entry point for application.
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static int Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(Configuration)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
-                .WriteTo.ApplicationInsightsWithStandardLoggersForTraceTelemetry(Configuration["ApplicationInsights.InstrumentationKey"])
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting web host");
-                CreateWebHostBuilder(args).Build().Run();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        internal static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .UseConfiguration(Configuration)
-                .UseSerilog();
+      try
+      {
+        Log.Information("Starting web host");
+        CreateWebHostBuilder(args).Build().Run();
+        return 0;
+      }
+      catch (Exception ex)
+      {
+        Log.Fatal(ex, "Host terminated unexpectedly");
+        return 1;
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
     }
+
+    internal static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseApplicationInsights()
+            .UseConfiguration(Configuration)
+            .UseSerilog();
+  }
 }
